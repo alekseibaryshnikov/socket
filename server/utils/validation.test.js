ce8f9a25ad59f7c0ca3a86961d5abd175aaf7e9c
Aleksei Baryshnikov <a250188@yandex.ru>
2018-06-05 09:18:44 +0300

    Added test for validtion.js with non-string

--- server/utils/validation.test.js
+++ server/utils/validation.test.js
@@ -1,15 +1,22 @@
 const { isRealString } = require('./validation.js');
 
 describe('- isRealString()', () => {
-  test('should return true', () => {
+  test('should allow string with non-spaces', () => {
     let str = 'Aleksei';
     let result = isRealString(str);
 
     expect(result).toBeTruthy();
   });
 
-  test('should return false', () => {
+  test('should reject string with only spaces', () => {
     let str = '   ';
+    let result = isRealString(str);
+
+    expect(result).toBeFalsy();
+  });
+
+  test('should reject non-string value', () => {
+    let str = 12345;
     let result = isRealString(str);
 
     expect(result).toBeFalsy();
